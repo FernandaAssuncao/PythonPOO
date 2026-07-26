886842d74d6f3426aa9aef9c02161ee3b64fefed
Fernanda assunção <fernandaassuncaosa@gmail.com>
2026-07-25 23:27:54 -0300

    Novos desafios, finalizados e em andamento.

---
+++ OOP/desafio_29/Diario.py
@@ -0,0 +1,21 @@
+from rich import print
+
+class Diario:
+    def __init__(self, senha='Fernanda'):
+        self.__senha = senha.strip()
+        self.__mensagems = []
+
+    def escrever(self, msg):
+        self.__mensagems.append(msg)
+
+    def ler(self, senha):
+        if senha.strip() == self.__senha:
+            print('[green]ACESSO PERMITIDO![/]')
+            for mensagem in self.__mensagems:
+                print(f':swan: - {mensagem}')
+        else:
+            print('[red]Senha incorreta! ACESSO NEGADO![/]')
+
+    @property
+    def senha(self):
+        raise PermissionError('Ninguem tem permissão de ver a senha!')
---
+++ OOP/desafio_29/__main__.py
@@ -0,0 +1,9 @@
+from Diario import Diario
+from rich import inspect
+
+if __name__ == '__main__':
+    p1 = Diario()
+    p1.escrever('O heitor é meu cavalheiro')
+    p1.escrever('Ele me trata como uma rainha')
+    p1.escrever('Ele me ama profundamente')
+    p1.ler('Fernanda')
